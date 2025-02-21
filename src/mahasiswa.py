a6ab61b9527cede76e09dd6feec1a7c723de475b
Putra <70789166+Dachenxi@users.noreply.github.com>
2025-02-21 19:44:57 +0700

    Add validation for NIM and enhance Mahasiswa class with update_prodi and ganti_dospem methods

--- src/mahasiswa.py
+++ src/mahasiswa.py
@@ -1,5 +1,7 @@
 class Mahasiswa:
     def __init__(self, nim, nama, prodi, angkatan, dospem):
+        if not nim.isdigit():
+            raise ValueError("NIM harus berupa angka.")
         self.nim = nim
         self.nama = nama
         self.prodi = prodi
@@ -7,4 +9,14 @@ class Mahasiswa:
         self.dospem = dospem
     
     def __str__(self):
-        return f"""Nim : {self.nim}\nNama : {self.nama}\nProdi : {self.prodi}\nAngkatan : {self.angkatan}\nDosen Pembimbing : {self.dospem}"""
+        return (f"NIM: {self.nim}\n"
+                f"Nama: {self.nama}\n"
+                f"Prodi: {self.prodi}\n"
+                f"Angkatan: {self.angkatan}\n"
+                f"Dosen Pembimbing: {self.dospem}")
+
+    def update_prodi(self, new_prodi):
+        self.prodi = new_prodi
+
+    def ganti_dospem(self, new_dospem):
+        self.dospem = new_dospem
